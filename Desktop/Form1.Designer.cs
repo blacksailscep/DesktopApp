03fe836d71716680bcfc713d0d2a7a05386c7ab2
christian lopez alegre <chrpriv@hotmail.com>
2019-02-26 16:48:31 +0100

    first commit christian

--- Desktop/Form1.Designer.cs
+++ Desktop/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(221, 255);
+            this.ClientSize = new System.Drawing.Size(435, 278);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
